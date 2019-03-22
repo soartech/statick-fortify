a58430876d63a211abf24f6221a4a77b96af74ab
Chris Reffett <chris.reffett@soartech.com>
2019-03-22 15:14:31 -0400

    refactor(tests): split tests into separate modules

--- tests/test_fortify_parse_output.py
+++ tests/test_fortify_parse_output.py
@@ -13,8 +13,6 @@ from statick_tool.plugin_context import PluginContext
 from statick_tool.plugins.tool.fortify_plugin.fortify_tool_plugin import \
     FortifyToolPlugin
 from statick_tool.resources import Resources
-from statick_tool.tool_plugin import ToolPlugin
-from yapsy.PluginManager import PluginManager
 
 
 def setup_fortify_tool_plugin():
@@ -33,25 +31,6 @@ def setup_fortify_tool_plugin():
     ftp = FortifyToolPlugin()
     ftp.set_plugin_context(plugin_context)
     return ftp
-
-
-def test_fortify_tool_plugin_found():
-    """Test that the fortify tool plugin is detected by the plugin system."""
-    manager = PluginManager()
-    # Get the path to statick_tool/__init__.py, get the directory part, and
-    # add 'plugins' to that to get the standard plugins dir
-    manager.setPluginPlaces([os.path.join(os.path.dirname(statick_tool.__file__),
-                                          'plugins')])
-    manager.setCategoriesFilter({
-        "Tool": ToolPlugin,
-    })
-    manager.collectPlugins()
-    # Verify that a plugin's get_name() function returns "fortify"
-    assert any(plugin_info.plugin_object.get_name() == 'fortify' for
-               plugin_info in manager.getPluginsOfCategory("Tool"))
-    # While we're at it, verify that a plugin is named Fortify Tool Plugin
-    assert any(plugin_info.name == 'Fortify Tool Plugin' for
-               plugin_info in manager.getPluginsOfCategory("Tool"))
 
 
 def sideeffect_python_found(*args, **kwargs):
---
+++ tests/test_fortify_plugin.py
@@ -0,0 +1,25 @@
+"""Unit tests for the fortify tool module."""
+import os
+
+import statick_tool
+from statick_tool.tool_plugin import ToolPlugin
+from yapsy.PluginManager import PluginManager
+
+
+def test_fortify_tool_plugin_found():
+    """Test that the fortify tool plugin is detected by the plugin system."""
+    manager = PluginManager()
+    # Get the path to statick_tool/__init__.py, get the directory part, and
+    # add 'plugins' to that to get the standard plugins dir
+    manager.setPluginPlaces([os.path.join(os.path.dirname(statick_tool.__file__),
+                                          'plugins')])
+    manager.setCategoriesFilter({
+        "Tool": ToolPlugin,
+    })
+    manager.collectPlugins()
+    # Verify that a plugin's get_name() function returns "fortify"
+    assert any(plugin_info.plugin_object.get_name() == 'fortify' for
+               plugin_info in manager.getPluginsOfCategory("Tool"))
+    # While we're at it, verify that a plugin is named Fortify Tool Plugin
+    assert any(plugin_info.name == 'Fortify Tool Plugin' for
+               plugin_info in manager.getPluginsOfCategory("Tool"))
