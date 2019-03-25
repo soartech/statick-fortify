e606a83bbcb88533c08eb65c4b6f6defd3bbd182
Chris Reffett <chris.reffett@soartech.com>
2019-03-25 11:41:01 -0400

    test(test_fortify_plugin): add test for _get_build_name

--- tests/test_fortify_plugin.py
+++ tests/test_fortify_plugin.py
@@ -4,6 +4,8 @@ import os
 from yapsy.PluginManager import PluginManager
 
 import statick_tool
+import statick_tool.plugins.tool.fortify_plugin.fortify_tool_plugin as ftp_module
+from statick_tool.package import Package
 from statick_tool.tool_plugin import ToolPlugin
 
 
@@ -24,3 +26,10 @@ def test_fortify_tool_plugin_found():
     # While we're at it, verify that a plugin is named Fortify Tool Plugin
     assert any(plugin_info.name == 'Fortify Tool Plugin' for
                plugin_info in manager.getPluginsOfCategory("Tool"))
+
+
+def test_fortify_get_build_name():
+    """Test the get_build_name function."""
+    package = Package('unusual-name', os.path.dirname(__file__))
+    assert ftp_module._get_build_name(package) == \
+        'statick-fortify-unusual-name'
